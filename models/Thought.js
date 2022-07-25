e7f05bffa1b9f19810eb9e4e5cfb70de3b693797
Kayla Rosario <rosariok314@gmail.com>
2022-07-25 00:04:08 -0400

    thought js requiring dependacies schema model with reaction schema, add thought text, creation username reactions get date function to add date of reac

---
+++ models/Thought.js
@@ -0,0 +1,42 @@
+const { Schema, model } = require("mongoose");
+const reactionSchema = require("./Reaction");
+
+const thoughtSchema = new Schema(
+  {
+    thoughtText: {
+      type: String,
+      required: true,
+      minLength: 1,
+      maxlength: 280,
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+      get: getDate,
+    },
+    username: {
+      type: String,
+      required: true,
+    },
+    reactions: [reactionSchema],
+  },
+  {
+    toJSON: {
+      getters: true,
+      virtuals: true,
+    },
+    id: false,
+  }
+);
+
+function getDate(date) {
+  return date;
+}
+
+thoughtSchema.virtual("reactionCount").get(function () {
+  return this.reactions.length;
+});
+
+const Thought = model("thought", thoughtSchema);
+
+module.exports = Thought;
